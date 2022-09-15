319fa606b6ac345ee98cbeacc63d6e20fa32a256
Abdullah <56775516+Zaazou@users.noreply.github.com>
2022-09-15 15:36:55 +0200

    finish

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ textarea.focus();
 
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value);
+    if(e.key === 'Enter'){
+        setTimeout(()=>{
+            e.target.value = '';
+        }, 10);
+        randomSelect();
+    
+    }
 });
 
 function createTags(input) {
@@ -18,3 +25,36 @@ function createTags(input) {
         tagsEL.appendChild(tagEl);
     })
 };
+
+function randomSelect(){
+    const times = 30;
+    const interval = setInterval(() => {
+        const  randomTag = pickRandomTag()
+        heighLightTag(randomTag);
+        setTimeout(() => {
+            unheighLightTag(randomTag);
+        },100);
+    },100);
+    setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+            heighLightTag(randomTag);
+        },100)
+    },times * 100)
+}
+
+function pickRandomTag(){
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function heighLightTag(tag){
+    tag.classList.add('heighlight');
+}
+
+
+function unheighLightTag(tag){
+    tag.classList.remove('heighlight');
+}
